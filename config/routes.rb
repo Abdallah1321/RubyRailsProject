1014c71f447fe3995734c7ca6d72295fd06ef702
abdallah <abdallah.ibrahim@tkh.edu.eg>
2022-05-25 10:06:41 +0200

    created db connection

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  resources :friends
+  #get 'home/index'
   get 'home/about'
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
